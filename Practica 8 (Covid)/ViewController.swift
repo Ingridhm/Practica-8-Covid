7442e11400fa989dbb520f4eec091f2449138777
ingridhm <ingrd.hm@gmail.com>
2020-12-03 18:50:45 -0600

    Initial Commit

---
+++ Practica 8 (Covid)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica 8 (Covid)
+//
+//  Created by Ingrid on 03/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
